6c873e5b7a57c59d74eff3acfaed68937af2d733
Morgan Vaterkowski <morgan.vaterkowski@epita.fr>
2021-10-11 00:06:13 +0200

    Revert "Put BleakDevice inside asyncio"
    This reverts commit ebf7c420aaff46e9bdcaf50b6d8cad43473303a0.

--- gateway/gateway_ammeter_influx.py
+++ gateway/gateway_ammeter_influx.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 
 from influxdb_client import InfluxDBClient, Point
@@ -20,7 +19,7 @@ class GatewayAmmeterInflux:
         self.__bucket = bucket
         # init ammeter connector
         self.__ammeter_connector = AmmeterConnector(BLEConnector())
-        self.__device_name = device_name
+        self.__ammeter_connector.connect(device_name)
         # is running
         self.__is_running = True
 
@@ -32,14 +31,8 @@ class GatewayAmmeterInflux:
         point: Point = Point("ampere_measurement").field("ampere", ammeter)
         self.__write_api.write(self.__bucket, self.__org, point)
 
-    def __read_ammeter_into_influx_forever(self):
-        self.__ammeter_connector.connect(self.__device_name)
+    def read_ammeter_into_influx_forever(self):
+        self.__logger.info("START GATEWAY FOR EVER")
         while self.__is_running:
             self.__read_ammeter_into_influx()
             time.sleep(1)
-
-    def read_ammeter_into_influx_forever(self):
-        self.__logger.info("START GATEWAY FOR EVER")
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.__read_ammeter_into_influx_forever())
-
