ebf7c420aaff46e9bdcaf50b6d8cad43473303a0
Morgan Vaterkowski <morgan.vaterkowski@epita.fr>
2021-10-10 23:52:32 +0200

    Put BleakDevice inside asyncio

--- gateway/gateway_ammeter_influx.py
+++ gateway/gateway_ammeter_influx.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from influxdb_client import InfluxDBClient, Point
@@ -19,7 +20,7 @@ class GatewayAmmeterInflux:
         self.__bucket = bucket
         # init ammeter connector
         self.__ammeter_connector = AmmeterConnector(BLEConnector())
-        self.__ammeter_connector.connect(device_name)
+        self.__device_name = device_name
         # is running
         self.__is_running = True
 
@@ -31,8 +32,14 @@ class GatewayAmmeterInflux:
         point: Point = Point("ampere_measurement").field("ampere", ammeter)
         self.__write_api.write(self.__bucket, self.__org, point)
 
-    def read_ammeter_into_influx_forever(self):
-        self.__logger.info("START GATEWAY FOR EVER")
+    def __read_ammeter_into_influx_forever(self):
+        self.__ammeter_connector.connect(self.__device_name)
         while self.__is_running:
             self.__read_ammeter_into_influx()
             time.sleep(1)
+
+    def read_ammeter_into_influx_forever(self):
+        self.__logger.info("START GATEWAY FOR EVER")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.__read_ammeter_into_influx_forever())
+
